ee8f8837231f22b4d533da1f71fb07bd061abf3e
CamilaFlorez12 <07camilaflorezsantos@gmail.com>
2025-10-27 18:57:31 -0500

    feat: :sparkles: config autentication
    config autentication JWT with passport.js
    
    By@CamilaFlorez

---
+++ src/config/contrasenia.js
@@ -0,0 +1,26 @@
+import passport, { use } from "passport";
+import { ExtractJwt,Strategy as JwtStrategy } from "passport-jwt";
+import dotenv from "dotenv";
+import { obtenerDB } from "./db";
+import { ObjectId } from "mongodb";
+
+dotenv.config();
+
+const opciones = {
+    secretOrKey: process.env.JWT_SECRET_KEY,
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+};
+
+passport.use(
+    new JwtStrategy(opciones, async (playLoad,done)=>{
+        try {
+            const user = await obtenerDB().collection("usuarios").findOne({_id:new ObjectId(playLoad.id)});
+            if(!user) return done(null, false);
+            return done(null,user);
+        } catch (error) {
+            done (error, false)
+        }
+    })
+)
+
+export default passport;
