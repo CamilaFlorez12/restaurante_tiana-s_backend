5127d91114542df67fd501ff44d49e86447721dd
CamilaFlorez12 <07camilaflorezsantos@gmail.com>
2025-10-27 19:03:44 -0500

    feat: :sparkles: register implementation
    register implementation of session with bcrypt and JWT
    
    By@CamilaFlorez

---
+++ src/services/usuarios.services.js
@@ -0,0 +1,57 @@
+import { obtenerDB } from "../config/db";;
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+const COLECCION_USUARIOS = "usuarios";
+
+function generarToken(usuario) {
+    return jwt.sign({ id:usuario._id?.toString(), rol:usuario.rol, correo:usuario.correo}, process.env.JWT_SECRET_KEY, { expiresIn: process.env.JWT_EXPIRES_TOKEN });
+}
+
+export async function registrarUsuario(datos) {
+    const { nombre, apellido, correo, contrasenia, rol } = datos;
+
+    if (!nombre || !apellido || !correo || !contrasenia || !rol) {
+        throw new Error("Falta algún campo");
+    }
+
+    const usuarioExistente = await obtenerDB().collection(COLECCION_USUARIOS).findOne({ correo });
+    if (usuarioExistente) throw new Error("Usuario ya existente");
+
+    const contraCifrada = await bcrypt.hash(contrasenia, parseInt(process.env.SALT_ROUNDS));
+
+    const rolFinal = rol === "admin" ? "usuario" : rol
+    
+
+    const usuario = {
+        nombre,
+        apellido,
+        correo,
+        contrasenia: contraCifrada,
+        rol:rolFinal
+    }
+    const resultado = await obtenerDB().collection(COLECCION_USUARIOS).insertOne(usuario);
+    const token = generarToken({_id:resultado.insertedId,rol:rolFinal,correo});
+    return { id: resultado.insertedId, nombre, apellido, correo, rol:rolFinal, token };
+}
+
+export async function iniciarSesion(correo,contrasenia) {
+      if (!correo || !contrasenia) throw new Error("Correo y contraseña son requeridos");
+    const usuarioExistente = await obtenerDB().collection(COLECCION_USUARIOS).findOne({correo});
+    if(!usuarioExistente) throw new Error("El usuario no exite");
+
+    const validacion = await bcrypt.compare(contrasenia,usuarioExistente.contrasenia);
+    if (!validacion) throw new Error("Constraseña incorrecta");
+
+    const token = generarToken(usuarioExistente);
+return {id: usuarioExistente._id,
+    name:usuarioExistente.nombre,
+    apellido:usuarioExistente.apellido,
+    correo:usuarioExistente.correo,
+    rol: usuarioExistente.rol,
+    token
+};
+}
