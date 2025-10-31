2fca61ca9471f0c0f0b093007dbb1fb24f9daa23
CamilaFlorez12 <07camilaflorezsantos@gmail.com>
2025-10-31 11:39:29 -0500

    fix: :bug: change required
    change required about dto
    
    By@CamilaFlorez

--- src/dtos/reseniasDTO.js
+++ src/dtos/reseniasDTO.js
@@ -2,10 +2,7 @@ import { body,param } from "express-validator";
 
 export const registrarReseniaDTO = [
     body("comentario").isString().trim().notEmpty().withMessage("El nombre es un campo obligatorio"),
-    body("calificacion").isInt().withMessage("la calificación debe ser un número entero"),
-    body("platoId").isString().trim().notEmpty().withMessage("El nombre es un campo obligatorio"),
-    body("usuarioId").isString().trim().notEmpty().withMessage("El nombre es un campo obligatorio"),
-]
+    body("calificacion").isInt().withMessage("la calificación debe ser un número entero"),]
 
 export const editarReseniaDTO = [
     body("comentario").optional().isString().trim().withMessage("El nombre es un campo obligatorio"),
